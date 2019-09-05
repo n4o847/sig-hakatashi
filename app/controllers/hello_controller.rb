b34d65b72fa9abb12a9fa9303f3a2d416939565f
n4o847 <yuu.miu.0925@gmail.com>
2019-09-05 14:54:33 +0900

    Add /days

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,4 +2,19 @@ class HelloController < ApplicationController
   def show
     @message = 'Hello, World!'
   end
+
+  def show_days
+    render action: 'days'
+  end
+
+  def calc_days
+    @year = params[:year].to_i
+    @month = params[:month].to_i
+    @day = params[:day].to_i
+
+    # ここに何か書く
+
+    @day_in_week = 'hoge曜日'
+    render action: 'days'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'hello#show'
+
+  get '/days', to: 'hello#show_days'
+  post '/days', to: 'hello#calc_days'
 end
